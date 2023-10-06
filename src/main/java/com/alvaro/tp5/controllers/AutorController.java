e1cd13c8753e95616d4f361b3789e5bd5e088f76
alvajius <alvajius@gmail.com>
2023-10-06 12:51:30 -0300

    ultimo-espero-

--- src/main/java/com/alvaro/tp5/controllers/AutorController.java
+++ src/main/java/com/alvaro/tp5/controllers/AutorController.java
@@ -1,5 +1,8 @@
 package com.alvaro.tp5.controllers;
 
+import com.alvaro.tp5.entities.Autor;
+import com.alvaro.tp5.services.AutorService;
+import com.alvaro.tp5.services.AutorServiceImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/basedatos/autores")
-public class AutorController {
+public class AutorController extends BaseControllerImpl<Autor, AutorServiceImpl> {
 }
